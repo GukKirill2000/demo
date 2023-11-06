a1b2163fcc53ba2aa1ea0c6125620e88d48ff37b
GukKirill2000 <GukKirill2000@example.com>
2023-11-06 12:56:31 +0000

    Replaced main.py with the version from demo_backup

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
->>>>>>> 138acb6ec0d2353f6a71650df33d80a3a5566463
 # Initialize an empty list 'a'
 a = []
 
 # Populate the list 'a' with numbers from 0 to 4
 for i in range(5):
-<<<<<<< HEAD
-    a.append(i)
-=======
   a.append(i)
 
 # Initialize an empty list 'b'
@@ -14,9 +10,22 @@ b = []
 
 # Iterate over the length of list 'a'
 for i in range(len(a)):
-<<<<<<< HEAD
-    # Convert the number to string and append ' I' to it
-    x = str(a[i]) + ' I'
-=======
   # Convert the number to lowercase (which will cause an error because numbers don't have a lowercase) and append ' I' to it
   x = a[i].lower()+' I'
+
+# Corrected code:
+
+# Initialize an empty list 'a'
+a = []
+
+# Populate the list 'a' with numbers from 0 to 4
+for i in range(5):
+    a.append(i)
+
+# Initialize an empty list 'b'
+b = []
+
+# Iterate over the length of list 'a'
+for i in range(len(a)):
+    # Convert the number to string and append ' I' to it
+    x = str(a[i]) + ' I'
